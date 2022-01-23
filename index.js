eaa6c628d3f1cca9563c0bffc81ad131c5aedec7
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2022-01-23 12:32:44 +0900

    nodemon 설치

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const AdminJSExpress = require('@adminjs/express')
 const express = require('express')
 const app = express()
 
-const adminJs = new AdminJS({databases: [], rootPath: '/admin'})
+const adminJs = new AdminJS({databases: [], rootPath: '/'})
 
 const router = AdminJSExpress.buildRouter(adminJs)
 
 app.use(adminJs.options.rootPath, router)
-app.listen(8080, () => console.log('AdminJS is under localhost:8080/admin'))
+app.listen(8080, () => console.log('AdminJS is under http://localhost:8080/'))

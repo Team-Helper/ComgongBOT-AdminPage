f339fc30aac2967c8c3f706646db3b82f6d7eb96
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2022-05-08 01:21:58 +0900

    #24 수정?

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,8 @@ module.exports = {
     entry: ['./build/js/index.js'],
     // The location of the build folder described above
     output: {
-        path: path.resolve(__dirname + '/build', 'dist'),
-        filename: 'bundle.js'
+        path: path.resolve(__dirname + '/build', 'lib'),
+        filename: 'bundle.js',
     },
     plugins: [new Dotenv()],
     // Optional and for development only. This provides the ability to map the built

38c0d1946672e1724a74bc5a7fd7c80037d5b4db
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2022-08-03 16:04:00 +0900

    #46 - init

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
+const path = require('path');
+
 module.exports = {
     runtimeCompiler: true,
     productionSourceMap: false,
+    outputDir: path.resolve(__dirname, "./build"),
 
     publicPath: process.env.NODE_ENV === 'production'
         ? './'

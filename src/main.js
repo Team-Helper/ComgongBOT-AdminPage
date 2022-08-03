cfbba12dea1f0fa1eb9184a9bd19be3df9a3f690
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2022-08-03 18:12:43 +0900

    #46 - sdk

--- src/main.js
+++ src/main.js
@@ -8,6 +8,30 @@ import App from './App'
 import Default from './Layout/Wrappers/baseLayout.vue';
 import Pages from './Layout/Wrappers/pagesLayout.vue';
 
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+// Import the functions you need from the SDKs you need
+import {initializeApp} from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries Your web app's
+// Firebase configuration For Firebase JS SDK v7.20.0 and later, measurementId
+// is optional
+const firebaseConfig = {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
 Vue.config.productionTip = false;
 
 Vue.use(BootstrapVue);
@@ -15,9 +39,6 @@ Vue.use(BootstrapVue);
 Vue.component('default-layout', Default);
 Vue.component('userpages-layout', Pages);
 
-new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
-});
+new Vue({el: '#app', router, template: '<App/>', components: {
+        App
+    }});

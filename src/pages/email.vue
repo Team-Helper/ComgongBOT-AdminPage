7bb5524b9342fbd83a8f9e29e690c1a7f616e5a7
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2022-08-22 00:53:33 +0900

    #47 - 각 요소별 타입과 설명 추가

--- src/pages/email.vue
+++ src/pages/email.vue
@@ -18,20 +18,23 @@
                                         id="exampleInputGroup1"
                                         label-for="exampleInput1"
                                         label="학교 이메일 주소"
-                                        label-class="font-weight-bold">
-                                        <b-form-input
-                                            id="exampleInput1"
-                                            v-model="form.email"
-                                            type="email"
-                                            required="required"
-                                            autofocus="autofocus"
-                                            placeholder="ex) test@sungkyul.ac.kr"></b-form-input>
+                                        label-class="font-weight-bold"
+                                        description="본인의 학교 아이디를 입력해주세요.">
+                                        <b-input-group append="@sungkyul.ac.kr">
+                                            <b-form-input
+                                                id="exampleInput1"
+                                                v-model="form.email"
+                                                type="text"
+                                                required="required"
+                                                autofocus="autofocus"></b-form-input>
+                                        </b-input-group>
                                     </b-form-group>
                                     <b-form-group
                                         id="exampleInputGroup2"
                                         label-for="exampleInput2"
                                         label="학년 선택"
-                                        label-class="font-weight-bold">
+                                        label-class="font-weight-bold"
+                                        description="본인의 학년을 선택해주세요.">
                                         <b-form-select v-model="form.grade" required="required">
                                             <b-form-select-option :value="null" disabled="disabled">-- 학년을 선택하세요 --</b-form-select-option>
                                             <b-form-select-option value="1">1학년</b-form-select-option>
@@ -44,12 +47,15 @@
                                         id="exampleInputGroup3"
                                         label-for="exampleInput3"
                                         label="학번 입력"
-                                        label-class="font-weight-bold">
+                                        label-class="font-weight-bold"
+                                        description="본인의 학번 숫자 2자리를 입력해주세요.">
                                         <b-form-input
                                             v-model="form.studentID"
                                             id="exampleInput3"
                                             type="number"
                                             required="required"
+                                            min="08"
+                                            max="22"
                                             placeholder="ex) 22"></b-form-input>
                                     </b-form-group>
                                 </div>
@@ -79,7 +85,7 @@
             methods: {
                 submit(event) {
                     event.preventDefault();
-                    console.log(this.form.email, this.form.grade, this.form.studentID);
+                    console.log(this.form);
                 }
             }
         };

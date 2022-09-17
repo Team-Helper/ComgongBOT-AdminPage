5c76bce8180b59b0323cbdba3834983265f8e969
leehyesong <laserjet4100@sungkyul.ac.kr>
2022-09-17 15:09:11 +0900

    #49
    수정본

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -18,22 +18,19 @@
                                         id="exampleInputGroup1"
                                         label-for="exampleInput1"
                                         label="아이디"
-                                        label-class="font-weight-bold"
-                                        description="관리자 아이디를 입력해 주세요.">
+                                        label-class="font-weight-bold">
                                         <b-form-input
                                             id="exampleInput1"
                                             v-model="form.id"
                                             type="text"
                                             required="required"
-                                            placeholder="admin"
                                             autofocus="autofocus"></b-form-input>
                                     </b-form-group>
                                     <b-form-group
                                         id="exampleInputGroup2"
                                         label-for="exampleInput2"
                                         label="비밀번호"
-                                        label-class="font-weight-bold"
-                                        description="관리자 비밀번호를 입력해 주세요.">
+                                        label-class="font-weight-bold">
                                         <b-form-input
                                             id="exampleInput2"
                                             v-model="form.password"
@@ -41,31 +38,30 @@
                                             required="required"
                                             placeholder="Enter password..."></b-form-input>
                                     </b-form-group>
-                                    <div class="divider"/>
-                                    <h6 class="mb-0">
-                                        <a href="javascript:void(0);" class="text-blue btn-lg btn btn-link">비밀번호를 잊으셨나요?</a>
-                                    </h6>
-                                </div>
-                                <div class="modal-footer clearfix">
-                                    <b-button type="submit" block="block" variant="blue" size="lg">Sign in</b-button>
-                                </div>
-                            </b-form>
+                                    <div class="mb-0">
+                                        <a href="javascript:void(0);" class="text-blue btn-link">비밀번호를 잊었습니까?</a>
+                                    </div>
+                                    <br></div>
+                                    <div class="modal-footer clearfix">
+                                        <b-button type="submit" block="block" variant="blue" size="lg">로그인</b-button>
+                                    </div>
+                                </b-form>
+                            </div>
                         </div>
-                    </div>
-                </b-col>
+                    </b-col>
+                </div>
             </div>
-        </div>
-    </template>
+        </template>
 
-    <script>
-        export default {
-            data() {
-                return {
-                    form: {
-                        id: '',
-                        password: ''
-                    }
-                };
-            }
-        };
-    </script>
+        <script>
+            export default {
+                data() {
+                    return {
+                        form: {
+                            id: '',
+                            password: ''
+                        }
+                    };
+                }
+            };
+        </script>

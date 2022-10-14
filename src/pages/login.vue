cc84301fab49fc631c36a3db530b66bd96d8a57e
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2022-10-14 16:44:52 +0900

    #62

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,6 +1,13 @@
 <template>
     <div>
         <div class="d-flex h-100 justify-content-center align-items-center">
+            <b-modal
+                v-model="loginFailed"
+                title="로그인 실패..."
+                ok-only="ok-only"
+                no-close-on-backdrop="no-close-on-backdrop"
+                hide-backdrop="hide-backdrop">올바른 패스워드나 아이디를 입력해주세요!
+            </b-modal>
             <b-col md="8" class="mx-auto app-login-box">
                 <div class="modal-dialog w-100 mx-auto">
                     <div class="modal-content">
@@ -35,8 +42,7 @@
                                             id="exampleInput2"
                                             v-model="form.password"
                                             type="password"
-                                            required="required"
-                                            placeholder="Enter password..."></b-form-input>
+                                            required="required"></b-form-input>
                                     </b-form-group>
                                     <div class="mb-0">
                                         <a href="javascript:void(0);" class="text-blue btn-link">비밀번호를 잊었습니까?</a>
@@ -54,14 +60,47 @@
     </template>
 
     <script>
+        import {getAuth, signInWithEmailAndPassword} from "firebase/auth";
         export default {
             data() {
                 return {
                     form: {
                         id: '',
                         password: ''
-                    }
+                    },
+                    loginFailed: false // 로그인 실패 UI 출력 Flag 값
                 };
+            },
+            methods: {
+                submit(event) {
+                    event.preventDefault();
+                    const auth = getAuth();
+                    const email = this.form.id + '@sungkyul.ac.kr';
+                    const password = this.form.password;
+                    /* 개발/배포 모드를 구분해 로그 출력 및 */
+                    if (process.env.NODE_ENV === 'development') {
+                        signInWithEmailAndPassword(auth, email, password)
+                            .then((result) => {
+                                console.log(result);
+                                alert('login success!');
+                            })
+                            .catch((err) => {
+                                console.error(err);
+                                this.loginFailed = true;
+                            });
+                    } else {
+                        signInWithEmailAndPassword(auth, email)
+                            .then(() => {
+                                this
+                                    .$router
+                                    .push('/index');
+                            })
+                            .catch(() => {
+                                this.loginFailed = true;
+                            });
+                    }
+
+                }
             }
         };
     </script>
